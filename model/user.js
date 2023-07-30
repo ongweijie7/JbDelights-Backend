b5e565ffeeb919d9abd1d50281a9df4bba0cc168
Wei Jie <ongweijie7@gmail.com>
2023-07-30 15:20:10 +0800

    Add admin field

--- model/user.js
+++ model/user.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
 });
   
 
